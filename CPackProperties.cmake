19bcfbdaec7bc54de9c15f42741a6399992387ac
embeddedt <42941056+embeddedt@users.noreply.github.com>
2021-01-31 17:21:55 +0000

    Deploying to gh-pages from  @ 56a9dc3d5a85b663de570a47d02b821aa85e92c8 🚀

--- CPackProperties.cmake
+++ CPackProperties.cmake
@@ -1,12 +1,12 @@
 # Make the current version available to CPack
-set(CPACK_PACKAGE_VERSION "20210131-stock_jgr-g274d0efbc7")
+set(CPACK_PACKAGE_VERSION "20210131-stock_jgr-g56a9dc3d5a")
 
 # Name the output file with the correct version
-string(REPLACE "#CPACK_PACKAGE_VERSION#" "20210131-stock_jgr-g274d0efbc7" CPACK_PACKAGE_FILE_NAME "${CPACK_PACKAGE_FILE_NAME}")
+string(REPLACE "#CPACK_PACKAGE_VERSION#" "20210131-stock_jgr-g56a9dc3d5a" CPACK_PACKAGE_FILE_NAME "${CPACK_PACKAGE_FILE_NAME}")
 
 if (CPACK_BUNDLE_PLIST_SOURCE)
     # Rewrite the Info.plist.in to contain the correct version
     file(READ ${CPACK_BUNDLE_PLIST_SOURCE} INFO_PLIST_CONTENT)
-    string(REPLACE "#CPACK_PACKAGE_VERSION#" "20210131-stock_jgr-g274d0efbc7" INFO_PLIST_CONTENT "${INFO_PLIST_CONTENT}")
+    string(REPLACE "#CPACK_PACKAGE_VERSION#" "20210131-stock_jgr-g56a9dc3d5a" INFO_PLIST_CONTENT "${INFO_PLIST_CONTENT}")
     file(WRITE ${CPACK_BUNDLE_PLIST} "${INFO_PLIST_CONTENT}")
 endif (CPACK_BUNDLE_PLIST_SOURCE)
--- generated/rev.cpp
+++ generated/rev.cpp
@@ -35,7 +35,7 @@ bool IsReleasedVersion()
  *
  * <modified> shows a "M", if the binary is made from modified source code.
  */
-const char _openttd_revision[] = "20210131-stock_jgr-g274d0efbc7";
+const char _openttd_revision[] = "20210131-stock_jgr-g56a9dc3d5a";
 
 /**
  * The text version of OpenTTD's build date.
@@ -53,7 +53,7 @@ const char _openttd_build_configure_defines[] = "NDEBUG WITH_DEMANGLE WITH_SIGAC
 /**
  * The git revision hash of this version.
  */
-const char _openttd_revision_hash[] = "274d0efbc722448a182da29b9119537306eb10b3";
+const char _openttd_revision_hash[] = "56a9dc3d5a85b663de570a47d02b821aa85e92c8";
 
 /**
  * The year of this version.
