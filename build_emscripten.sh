c139802e1813f171a5c8a5a6beb0da73723a9061
Themba Dube <embeddedthemba@gmail.com>
2021-05-07 11:37:20 -0400

    Remove old Emscripten build logic
    Upstream OpenTTD now supports Emscripten builds directly.

--- build_emscripten.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-./configure --with-zlib --without-lzo2 --without-sse --without-lzma --without-threads --enable-dedicated
-make -C objs/lang all
-make -C objs/setting all
-emconfigure sh -c './configure --ignore-extra-parameters --without-lzo2 --without-sse --without-threads --with-zlib --without-lzma --with-sigaction="0" --with-sigaltstack="0"' CFLAGS="-s USE_ZLIB=1" LDFLAGS="-s USE_ZLIB=1"
-emmake make -j$(nproc)
